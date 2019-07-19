3d5296bca24ed6baee00c77b52fb9a408111ec24
arthurcgc <arthur.cavalcante.puc@gmail.com>
2019-07-19 14:13:17 -0300

    minor addtions to the script

--- wpp_down.py
+++ wpp_down.py
@@ -2,9 +2,17 @@ import requests
 import bs4 as bs
 import re
 import urllib.request
+import shutil
+import os
 
+dir_name = "downloads"
 
-url = "https://unsplash.com/wallpaper/1065396/desktop-wallpapers"
+if os.path.exists(dir_name):
+    shutil.rmtree(dir_name)
+
+os.mkdir(dir_name)
+
+url = "https://unsplash.com/t/wallpapers"
 
 resp = requests.get(url)
 
@@ -17,7 +25,7 @@ for tag in l:
     links.append(tag['href'])
 
 for i in range(0, len(links)):
-    name = "wpp" + str(i) + ".jpeg"
+    name = "downloads/wpp" + str(i) + ".jpeg"
     urllib.request.urlretrieve(links[i], name)
 
 
